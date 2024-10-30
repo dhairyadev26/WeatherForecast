0289a6d61f363a713de74ec1cdc737b9f9924d50
niyati34 <niyati4201@gmail.com>
2024-10-30 20:04:37 +0530

    Redu

--- src/test/__tests__/testReducers.js
+++ src/test/__tests__/testReducers.js
@@ -3,7 +3,6 @@ import React from "react";
 import * as actions from "../../actions/weatherStation";
 import reducer from "../../reducers";
 
-import { FETCH_DATA_FULFILLED, FETCH_DATA_REJECTED } from "../../constants/ActionTypes";
 
 import mockData from "./data/forecast.json";
 
