97648c24e5ee4186028c4ee9dae07f542c345dcd
niyati34 <niyati4201@gmail.com>
2024-11-03 13:17:23 +0530

    Refactor login

--- src/components/ForecastTiles.js
+++ src/components/ForecastTiles.js
@@ -9,6 +9,7 @@ export default class ForecastTiles extends Component {
       const forecastDate = item.dt_txt.substr(0,10);
       list[forecastDate] = list[forecastDate] || [];
       list[forecastDate].push(item);
+
       return list;
     }, {}));
   };
