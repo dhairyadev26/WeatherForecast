e5def3676db7154cfbe988d31161595248c8792f
niyati34 <niyati4201@gmail.com>
2025-01-08 19:47:09 +0530

    //

--- src/components/WeatherForecast.js
+++ src/components/WeatherForecast.js
@@ -11,6 +11,7 @@ const WeatherForecast = ({ data }) => {
   
     return (
       <div className="weather-forecast-wrapper">
+        <Dashboard city={name} />
         <ForecastTiles forecasts={list} />
       </div>
     );
