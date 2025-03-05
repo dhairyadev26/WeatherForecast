f55ad8b439856f4ab0fcf6a607b5962a849a330d
niyati34 <niyati4201@gmail.com>
2025-03-05 11:53:22 +0530

    Refactor login

--- src/constants/ActionTypes.js
+++ src/constants/ActionTypes.js
@@ -3,9 +3,7 @@
 // They are imported in Actions and Reducers.
 // This prevents errors if they are misspelled over here.
 
-/* eslint-disable */
-
-export const FETCH_DATA_FULFILLED = "FETCH_DATA_FULFILLED";
+/* eslint-disable onst FETCH_DATA_FULFILLED = "FETCH_DATA_FULFILLED";
 export const FETCH_DATA_REJECTED = "FETCH_DATA_REJECTED";
 
 /* eslint-enable */
