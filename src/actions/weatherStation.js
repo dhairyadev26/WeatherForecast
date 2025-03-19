208590531c712506828a7e03e6080eb6d5d1e01d
niyati34 <niyati4201@gmail.com>
2025-03-19 20:46:53 +0530

    api

--- src/actions/weatherStation.js
+++ src/actions/weatherStation.js
@@ -7,7 +7,6 @@ export const fetchData = (region) => (dispatch) => {
   const { latitude, longitude } = region || {};
 
   const getDataByCity = `https://api.openweathermap.org/data/2.5/forecast?q=${region}&units=metric&appid=${APP_ID}`;
-  const getDataByCoords = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&units=metric&appid=${APP_ID}`;
 
   let location = typeof(region) === "object" ? getDataByCoords : getDataByCity;
 
